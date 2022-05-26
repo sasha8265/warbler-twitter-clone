035ddecc7c590595b1eac124670922ff227d95a9
Sasha Czerniawski <sashacandoit@gmail.com>
2022-05-26 07:05:07 -0400

    starting user model tests

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask, render_template, request, flash, redirect, session, g, abort
-from flask_debugtoolbar import DebugToolbarExtension
+# from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 
 from forms import UserAddForm, LoginForm, MessageForm, UserUpdateForm
@@ -17,9 +17,9 @@ app.config['SQLALCHEMY_DATABASE_URI'] = (
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = False
-app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+# app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', "it's a secret")
-toolbar = DebugToolbarExtension(app)
+# toolbar = DebugToolbarExtension(app)
 
 connect_db(app)
 
--- test_user_model.py
+++ test_user_model.py
@@ -14,18 +14,15 @@ from models import db, User, Message, Follows
 # to use a different database for tests (we need to do this
 # before we import our app, since that will have already
 # connected to the database
-
-os.environ['DATABASE_URL'] = "postgresql:///warbler-test"
+os.environ['DATABASE_URL'] = "postgresql:///warbler_test"
 
 
 # Now we can import app
-
 from app import app
 
 # Create our tables (we do this here, so we only create the tables
 # once for all tests --- in each test, we'll delete the data
 # and create fresh new clean test data
-
 db.create_all()
 
 
@@ -56,3 +53,56 @@ class UserModelTestCase(TestCase):
         # User should have no messages & no followers
         self.assertEqual(len(u.messages), 0)
         self.assertEqual(len(u.followers), 0)
+
+
+    def test_is_following(self):
+        """Does is_following detect when user1 is following user2?"""
+
+        u1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        u2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        followed = User.query.get(u2.id)
+        u1.following.append(followed)
+        db.session.commit()
+
+        self.assertEqual(len(u2.followers), 1)
+        self.assertEqual(len(u1.following), 1)
+        self.assertTrue(u1.is_following(u2))
+
+
+
+    def test_not_following(self):
+        """Does is_following detect when user1 is NOT following user2?"""
+
+        u1 = User(
+            email="test1@test.com",
+            username="testuser1",
+            password="HASHED_PASSWORD"
+        )
+
+        u2 = User(
+            email="test2@test.com",
+            username="testuser2",
+            password="HASHED_PASSWORD"
+        )
+
+        db.session.add(u1)
+        db.session.add(u2)
+        db.session.commit()
+
+        self.assertEqual(len(u2.followers), 0)
+        self.assertEqual(len(u1.following), 0)
+        self.assertFalse(u1.is_following(u2))
